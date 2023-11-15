a8148d90ee1aa8cf5edec65de79209d099b0c629
Khazixi <khazixfangirl@gmail.com>
2023-11-15 17:13:04 -0500

    Error Page Shipped. Thanks Elysia Discord

--- src/templates.tsx
+++ src/templates.tsx
@@ -124,13 +124,13 @@ type ErrorProps = {
 
 export const ErrorTemplate = (props: ErrorProps) => (
   <article class="flex flex-col rounded-md shadow-lg w-96 gap-8 mx-auto mt-28">
-    <header class="bg-red-300 basis-1/3 rounded-t-md">
+    <header class="bg-red-300 rounded-t-md h-12 flex flex-col justify-center items-center">
       <h1 class="text-red-700 font-extrabold text-2xl text-center"> An Error Occured </h1>
     </header>
 
-    <section class="flex flex-col gap-4 basis-2/3 p-4">
-      <h2 class="text-xl font-semibold"> Details </h2>
-      <p> Error: <b class="font-bold"> {props.error.message} </b></p>
+    <section class="flex flex-col gap-4 p-4">
+      <h2 class="text-xl font-semibold text-gray-800"> Details: </h2>
+      <p class="text-gray-600"> Error: <b class="font-bold text-black"> {props.error.message} </b></p>
     </section>
   </article>
 )
